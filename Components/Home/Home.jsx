2b04832e2b6236388f7d775d4504c6ff738601cc
Shujaat Ahmed <ahmed.shujaat555@gmail.com>
2024-04-23 20:55:33 +0500

    react caraousel

---
+++ Components/Home/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ResponsiveCarousel = () => {
+  return (
+    <div className="container">
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/800x400"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First Slide</h3>
+            <p>Description for the first slide.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/800x400"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second Slide</h3>
+            <p>Description for the second slide.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/800x400"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third Slide</h3>
+            <p>Description for the third slide.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default ResponsiveCarousel;
